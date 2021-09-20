dd75d95b99d4d557451d6abbb8036cd014d86167
sayem254 <sayem254@gmail.com>
2021-09-20 18:33:27 +0200

    final commit

--- books.js
+++ books.js
@@ -12,17 +12,17 @@ const searchBook = () => {
 
 const bookSearchResult = books => {
     const searchResult = document.getElementById('search-result');
-    // searchResult.textContent = '';
+    searchResult.textContent = '';
     books.forEach(book => {
-
         const div = document.createElement('div')
-        const imgUrl = "https://covers.openlibrary.org";
-        console.log(imgUrl);
+        // const imgUrl = 
+        // console.log(imgUrl);
+
         div.classList.add('col');
         div.innerHTML = `
         <div class="card h-100 ">
                 <div class="card-body">
-                    <img src="" class="card-img-top" alt="...">
+                    <img src="https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg" class="card-img-top" alt="...">
                     <h4 class="card-title ">${book.subject} </h4>    
                     <h6 class="card-title "> Author : ${book.author_name} </h4>
                     <h6 class="card-title ">First Published : ${book.first_publish_year} </h6>
